6901508ec37b0f75301b7d8b096dfa095b726bc4
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-09 22:35:17 +0300

    4.1.2

---
+++ ex_2/script.js
@@ -0,0 +1,11 @@
+let schedule = {};
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    } return true;
+}
+
+schedule[name] = 'Alex';
+
+console.log(isEmpty(schedule)); //работает, если цикл пошел, значит свойство есть, значит false
